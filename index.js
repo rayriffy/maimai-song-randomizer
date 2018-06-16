405d4192dd2e20cfd9b69f9fc66f7ea711be983c
Phumrapee Limpianchop <rayriffy@gmail.com>
2018-06-16 15:33:23 +0700

    ✨ Added configuration inside the script

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/**
+ *  GET NECESSARY LIBRARY
+ */
+
 const { dialogflow } = require('actions-on-google');
 const { Image, Suggestions, LinkOutSuggestion, BrowseCarousel, BrowseCarouselItem, SimpleResponse } = require('actions-on-google');
 const request = require('request')
@@ -7,6 +11,27 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const rp = require('request-promise');
 const i18n = require('i18n');
+
+/**
+ *  CONFIGURATION
+ */ 
+
+const config = {
+  local: true
+
+};
+
+/**
+ *  PROCESS
+ */
+
+var endpoint;
+
+if(config.local) {
+  endpoint = 'http://maimaibot.local';
+} else {
+  endpoint = 'https://maimaibot.rayriffy.com';
+}
 
 i18n.configure({
   locales: ['en-US', 'ja-JP'],
@@ -24,7 +49,7 @@ app.middleware((conv) => {
 app.intent('random-niconico', (conv) => {
   console.log('\x1b[33minfo:\x1b[0m Hit niconico & VOCALOID intent');
   var options = {
-    uri: 'http://maimaibot.local/nico.json',
+    uri: endpoint + '/nico.json',
     headers: {
         'User-Agent': 'Request-Promise'
     },
@@ -39,7 +64,7 @@ app.intent('random-niconico', (conv) => {
 app.intent('random-anime', (conv) => {
   console.log('\x1b[33minfo:\x1b[0m Hit POPS & ANIME intent');
   var options = {
-    uri: 'http://maimaibot.local/pops.json',
+    uri: endpoint + '/pops.json',
     headers: {
         'User-Agent': 'Request-Promise'
     },
@@ -54,7 +79,7 @@ app.intent('random-anime', (conv) => {
 app.intent('random-original', (conv) => {
   console.log('\x1b[33minfo:\x1b[0m Hit ORIGINAL & JOYPOLIS intent');
   var options = {
-    uri: 'http://maimaibot.local/orig.json',
+    uri: endpoint + '/orig.json',
     headers: {
         'User-Agent': 'Request-Promise'
     },
@@ -69,7 +94,7 @@ app.intent('random-original', (conv) => {
 app.intent('random-sega', (conv) => {
   console.log('\x1b[33minfo:\x1b[0m Hit SEGA intent');
   var options = {
-    uri: 'http://maimaibot.local/sega.json',
+    uri: endpoint + '/sega.json',
     headers: {
         'User-Agent': 'Request-Promise'
     },
@@ -84,7 +109,7 @@ app.intent('random-sega', (conv) => {
 app.intent('random-game', (conv) => {
   console.log('\x1b[33minfo:\x1b[0m Hit GAMES & VARIETY intent');
   var options = {
-    uri: 'http://maimaibot.local/game.json',
+    uri: endpoint + '/game.json',
     headers: {
         'User-Agent': 'Request-Promise'
     },
@@ -99,7 +124,7 @@ app.intent('random-game', (conv) => {
 app.intent('random-touhou', (conv) => {
   console.log('\x1b[33minfo:\x1b[0m Hit TOUHOU Project intent');
   var options = {
-    uri: 'http://maimaibot.local/toho.json',
+    uri: endpoint + '/toho.json',
     headers: {
         'User-Agent': 'Request-Promise'
     },
@@ -133,10 +158,10 @@ function showCardorSpeak(detail,conv) {
     var cards = [];
     var is_regionlocked=null;
     for(var i = 0; i < detail.length; i++) {
-      if(detail[i].regionlocked==1) {
-        is_regionlocked=1;
+      if(detail[i].regionlocked == 1) {
+        is_regionlocked = 1;
       }
-      if(conv.user.locale=="en-US") {
+      if(conv.user.locale == "en-US") {
         detail[i].name = detail[i].name_en;
         detail[i].artist = detail[i].artist_en;
       } else {
@@ -150,28 +175,28 @@ function showCardorSpeak(detail,conv) {
           url: detail[i].image_url,
           alt: 'Image',
         }),
-        description: i18n.__('RESULT_CARD_AUTHOR')+': '+detail[i].artist+' | '+i18n.__('RESULT_CARD_INTRODUCED')+': '+detail[i].version,
+        description: i18n.__('RESULT_CARD_AUTHOR') + ': ' + detail[i].artist + ' | ' + i18n.__('RESULT_CARD_INTRODUCED') + ': ' + detail[i].version,
       }));
     }
     var action_rlock_text,action_rlock_speech;
-    if(is_regionlocked==1 && conv.user.locale!="ja-JP") {
+    if(is_regionlocked == 1 && conv.user.locale != 'ja-JP') {
       action_rlock_text = i18n.__('RESULT_TEXT_REGIONLOCKED');
       action_rlock_speech = i18n.__('RESULT_SPEAK_REGIONLOCKED');
     }
     else {
-      action_rlock_text="";
-      action_rlock_speech="";
+      action_rlock_text = "";
+      action_rlock_speech = "";
     }
     conv.ask(new SimpleResponse({
-      speech: '<speak>'+i18n.__('RESULT_SPEAK')+action_rlock_speech+'</speak>',
-      text: i18n.__('RESULT_SPEAK')+action_rlock_text,
+      speech: '<speak>' + i18n.__('RESULT_SPEAK') + action_rlock_speech + '</speak>',
+      text: i18n.__('RESULT_SPEAK') + action_rlock_text,
     }));
     conv.ask(new BrowseCarousel({items: cards}));
     conv.ask(new Suggestions([i18n.__('SUGGESTION_CARD_CANCEL'), i18n.__('SUGGESTION_CARD_POPS'), i18n.__('SUGGESTION_CARD_NICO'), i18n.__('SUGGESTION_CARD_TOHO'), i18n.__('SUGGESTION_CARD_SEGA'), i18n.__('SUGGESTION_CARD_GAME'), i18n.__('SUGGESTION_CARD_ORIG')]));
   } else {
     let speak = "<speak><p>";
     for(var i = 0; i < detail.length; i++) {
-      if(conv.user.locale=="en-US") {
+      if(conv.user.locale == 'en-US') {
         detail[i].name = detail[i].name_en;
       } else {
         detail[i].name = detail[i].name_jp;
